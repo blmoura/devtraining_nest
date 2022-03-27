a686bfedb1d9355c182552e2eb90a9b5c16d744f
Bruno Moura <brunomoura.blm@gmail.com>
2022-03-27 02:00:54 -0300

    downgrade typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [
-    CoursesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'db_devtraining',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [CoursesModule],
 })
 export class AppModule {}
