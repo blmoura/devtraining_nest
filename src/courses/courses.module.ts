dfb3a08c1778f4ce8564670286b4c9cac9eeb62d
Bruno Moura <brunomoura.blm@gmail.com>
2022-03-26 21:33:06 -0300

    initial commit

---
+++ src/courses/courses.module.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoursesService } from './courses.service';
+import { CoursesController } from './courses.controller';
+import { Module } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Course, Tag])],
+  controllers: [CoursesController],
+  providers: [CoursesService],
+})
+export class CoursesModule {}
